822c341fd78057168b3eed2b177d93d673c70e4b
ZENO <zeno1030@naver.com>
2024-02-06 13:39:03 +0900

    init setting

--- sample/src/main/java/study/sample/InvalidOperatorException.java
+++
@@ -1,8 +0,0 @@
-package study.sample;
-
-public class InvalidOperatorException extends RuntimeException{
-
-    public InvalidOperatorException() {
-        super("Invaild operator, you need to choose one of (+,-,*,/)");
-    }
-}

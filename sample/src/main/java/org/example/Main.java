1d702a6dd815bfdbc42e703ba4223c99ce8aeb64
ZENO <zeno1030@naver.com>
2024-02-06 11:25:06 +0900

    added scanner

--- sample/src/main/java/org/example/Main.java
+++ sample/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
 	}
 }

71d592e6ad39c0b60221fae2bb78c0a9e7161266
ZENO <zeno1030@naver.com>
2024-02-27 12:13:28 +0900

    @DisplayName을 사용하여 테스트 문서화를 잘해야한다

--- cafekiosk/src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -3,10 +3,11 @@ package sample.cafekiosk.unit.beverage;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AmericanoTest {
+	@DisplayName("아메리카노 하나를 추가하면 장바구니에 담긴다")
 	@Test
 	void getName() {
 		Americano americano = new Americano();
